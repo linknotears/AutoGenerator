fe14aea0958ecbda5442e3efa66f08cc17f097e3
linknotears <woshixiaozhu022@qq.com>
2019-10-27 17:42:44 +0800

    增加列表查询添加额外参数

--- src/main/resources/templates/web/not-analyze/common/util/list.js
+++ src/main/resources/templates/web/not-analyze/common/util/list.js
@@ -3,7 +3,10 @@ $(function() {
 		load();
 	});
 });
-
+var queryParams = {};
+function addParams(data){
+	Object.assign(queryParams,data);
+}
 function load() {
 	
 	$('#exampleTable')
@@ -23,20 +26,21 @@ function load() {
 						singleSelect : false, // 设置为true将禁止多选
 						// contentType : "application/x-www-form-urlencoded",
 						// //发送到服务器的数据编码类型
-						pageSize : 10, // 如果设置了分页，每页数据条数
+						pageSize : 5, // 如果设置了分页，每页数据条数
 						pageNumber : 1, // 如果设置了分布，首页页码
 						// search : true, // 是否显示搜索框
 						showColumns : false, // 是否显示内容下拉框（选择显示的列）
 						sidePagination : "server", // 设置在哪里进行分页，可选值为"client" 或者
 													// "server"
 						queryParams : function(params) {
-							return {
+							Object.assign(queryParams,{
 								// 说明：传入后台的参数包括offset开始索引，limit步长，sort排序列，order：desc或者,以及所有列的键值对
 								limit : params.limit,
 								offset : params.offset,
 							// name:$('#searchName').val(),
 							// username:$('#searchName').val()
-							};
+							});
+							return queryParams;
 						},
 						// //请求服务器数据时，你可以通过重写参数的方式添加一些额外的参数，例如 toolbar 中的参数 如果
 						// queryParamsType = 'limit' ,返回参数必须包含
@@ -53,7 +57,7 @@ function reLoad() {
 function add() {
 	layer.open({
 		type : 2,
-		title : '添加',
+		title : '增加',
 		maxmin : true,
 		shadeClose : false, // 点击遮罩关闭层
 		area : [ '800px', '520px' ],
