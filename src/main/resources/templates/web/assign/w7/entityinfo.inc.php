f934812186166014f5f813a4340c6bdc1263c4bb
linknotears <woshixiaozhu022@qq.com>
2019-11-22 22:03:09 +0800

    tttttttttt

--- src/main/resources/templates/web/assign/w7/entityinfo.inc.php
+++ src/main/resources/templates/web/assign/w7/entityinfo.inc.php
@@ -1,77 +1,48 @@
 <?php
+## 首字母小写
+#macro(d)$#end
+#macro(tolowercase $str)$str.substring(0,1).toLowerCase()$str.substring(1)#end
+##找出第一个id
+#foreach($field in ${table.fields})
+#if(${field.keyFlag})
+#set( $propId = $field.propertyName )
+#set( $colId = $field.name )
+#break
+#end
+#end
+#if(!$colId)
+#set( $propId = $table.fields[0].propertyName )
+#set( $colId = $table.fields[0].name )
+#end
+#set($shortName = ${table.name.replace($cfg.tablePrefix,'')})
 global $_GPC, $_W;
-$GLOBALS['frames'] = $this->getMainMenu();
-$info = pdo_get('store',array('id'=>$_GPC['id']));
+$GLOBALS['frames'] = #d()this->getMainMenu();
+$info = pdo_get("$shortName",array('id'=>$_GPC['id']));
 //二维数组转一维
 
-if($info['ad']){
-    if(strpos($info['ad'], ',')){
-        $ad= explode(',', $info['ad']);
-    }else{
-        $ad=array(0 => $info['ad']);
-    }
-}
-/*	 $coordinates=explode(',',$info['coordinates']);
- $list['coordinates']=array(
- 'lat'=>$coordinates['0'],
- 'lng'=>$coordinates['1'],
- ); */
 if(checksubmit('submit')){
-    if($_GPC['ad']) {
-        $data['ad'] = implode(",",$_GPC['ad']);
-    } else {
-        $data['ad'] = '';
+#foreach($field in ${table.fields})
+#if(!${field.keyFlag})
+    if(!empty($_GPC['$field.name'])){
+        $data['$field.name'] = $_GPC['$field.name'];
     }
-    if(empty($_GPC['user_id'])){
-        message('没有绑定管理员,请绑定后提交','','error');
-    }
-    //查询钱
-    $data['store_name'] = $_GPC['store_name'];
-    $data['tel'] = $_GPC['tel'];
-    $data['address'] = $_GPC['address'];
-    $data['logo'] = $_GPC['logo'];
-    $data['details'] = $_GPC['details'];
-    $data['coordinates'] = $_GPC['coordinates'];
-    $data['user_name'] = $_GPC['user_name'];
-    $pwd = $_GPC['pwd'];
-    if($pwd!='' && $pwd!=null){        
-        $data['pwd'] = md5($pwd);
-    }
-    $data['user_id'] = $_GPC['user_id'];
-    $data['wxuser_name'] = $_GPC['wxuser_name'];
-    $data['yyzz_img'] = $_GPC['yyzz_img'];
-    $data['start_time'] = $_GPC['start_time'];
-    $data['end_time'] = $_GPC['end_time'];
-    $data['state'] = 2;
-    $data['views'] = $_GPC['views'];
-    
-    //额外的
-    $data['least_price'] = $_GPC['least_price'];//起送价
-    $data['delivery_time'] = $_GPC['delivery_time'];//配送时间/分钟
-    $data['join_switch'] = $_GPC['join_switch'];//加盟开关
-    $data['partner_switch'] = $_GPC['partner_switch'];//入股开关
-    $data['is_recommend'] = $_GPC['is_recommend'];//品牌推荐开关
-    $data['set_yptg'] = $_GPC['set_yptg'];//优品推广开关
-    $data['set_fxhd'] = $_GPC['set_fxhd'];//发现好店开关
-    $data['fare'] = $_GPC['fare'];//运费
-    //$data['apply_time'] = time();//入驻时间
-    //$data['sh_time'] = time();//审核时间
-    
+#end
+#end
     if($_GPC['id'] == ''){
-        $res = pdo_insert('store', $data);
+        $res = pdo_insert('$shortName', $data);
         //pdo_debug();exit;
         if($res) {
-            message('新增成功',$this->createWebUrl('store',array('type'=>'all')),'success');
+            message('新增成功',$this->createWebUrl('#tolowercase($entity)',array('type'=>'all')),'success');
         } else {
             message('新增失败','','error');
         }
     }else {
-        $res = pdo_update('store', $data,array('id'=>$_GPC['id']));
+        $res = pdo_update('$shortName', $data,array('id'=>$_GPC['id']));
         if($res) {
-            message('编辑成功', $this->createWebUrl('store',array('type'=>'all')),'success');
+            message('编辑成功', $this->createWebUrl('#tolowercase($entity)',array('type'=>'all')),'success');
         } else {
             message('编辑失败', '', 'error');
         }
     }
 }
-include $this->template('web/storeinfo2');
+include $this->template('web/#tolowercase($entity)');
--- src/main/resources/templates/web/assign/w7/list.inc.php
+++ src/main/resources/templates/web/assign/w7/list.inc.php
@@ -1,66 +1,46 @@
 <?php
+## 首字母小写
+#macro(d)$#end
+#macro(tolowercase $str)$str.substring(0,1).toLowerCase()$str.substring(1)#end
+##找出第一个id
+#foreach($field in ${table.fields})
+#if(${field.keyFlag})
+#set( $propId = $field.propertyName )
+#set( $colId = $field.name )
+#break
+#end
+#end
+#if(!$colId)
+#set( $propId = $table.fields[0].propertyName )
+#set( $colId = $table.fields[0].name )
+#end
+#set($shortName = ${table.name.replace($cfg.tablePrefix,'')})
 global $_GPC, $_W;
 $GLOBALS['frames'] = $this->getMainMenu();
-$type = isset($_GPC['type'])?$_GPC['type']:'wait';
-$state = $_GPC['state'];
-if($type=='wait'){
-  $state = 1;
-}
 $where = "";
 //搜索
-if(isset($_GPC['keywords'])){
-  $where.=" and (a.store_name LIKE  concat('%', :name,'%') || a.tel LIKE  concat('%', :name,'%')) ";
+/*if(isset($_GPC['keywords'])){
+  $where .= " and (a.store_name LIKE  concat('%', :name,'%') || a.tel LIKE  concat('%', :name,'%')) ";
   $data[':name']=$_GPC['keywords'];
   $type = 'all'; 
-}else{
-  if($state){
-      $where.=" where a.state = :state ";
-      $data[':state'] = $state;
-  }
-}
+}*/
 
 $pageindex = max(1, intval($_GPC['page']));
 $pagesize=10;
 
-
-$sql="SELECT a.* FROM " . tablename('store') . " a" . $where . " ORDER BY a.id DESC";
-$total=pdo_fetchcolumn("select count(*) from " .tablename('store'). " a" . $where , $data );
-//echo $sql;die;
+$sql = "SELECT a.* FROM " . tablename('$shortName') . " a" . $where . " ORDER BY a.id DESC";
+$total = pdo_fetchcolumn("select count(1) from " . tablename('$shortName') . " a" . $where , $data );
 $select_sql = $sql . " LIMIT " .($pageindex - 1) * $pagesize . "," . $pagesize;
 
 $list = pdo_fetchall($select_sql, $data);
 
 $pager = pagination($total, $pageindex, $pagesize);
 if($_GPC['op']=='delete'){
-    $res=pdo_delete('store',array('id'=>$_GPC['id']));
+    $res=pdo_delete('$shortName',array('id'=>$_GPC['id']));
     if($res){
-         message('删除成功！', $this->createWebUrl('store'), 'success');
+         message('删除成功！', $this->createWebUrl('#tolowercase($entity)'), 'success');
     }else{
          message('删除失败！','','error');
     }
 }
-if($_GPC['op']=='tg'){
-    $rst=pdo_get('store',array('id'=>$_GPC['id']));
-    if(!$rst['sh_time']){//增加
-        $time=9999999999;
-        $time2=time();
-        $res = pdo_update('store', array('state'=>2, 'sh_time'=>$time2, 'dq_time'=>$time), array('id'=>$_GPC['id']));
-    }else{//修改
-        $res = pdo_update('store', array('state'=>2), array('id'=>$_GPC['id']));
-    }
-    if($res){
-        message('通过成功！', $this->createWebUrl('store'), 'success');
-    }else{
-        message('通过失败！','','error');
-    }
-}
-if($_GPC['op'] == 'jj'){
-    $res = pdo_update('store', array('state' => 3, 'sh_time' => time()),array('id' => $_GPC['id']));
-    if($res){
-        message('拒绝成功！', $this->createWebUrl('store'), 'success');
-    }else{
-        message('拒绝失败！','','error');
-    }
-}
-
-include $this->template('web/store');
+include $this->template('web/#tolowercase($entity)');
