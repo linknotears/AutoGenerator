e16466b1c95476627944641516ca78bce51b3604
linknotears <woshixiaozhu022@qq.com>
2019-11-24 15:50:12 +0800

    吞吞吐吐

--- src/main/java/cn/linkey/generator/GeneratorCode.java
+++ src/main/java/cn/linkey/generator/GeneratorCode.java
@@ -1,7 +1,5 @@
 package cn.linkey.generator;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
